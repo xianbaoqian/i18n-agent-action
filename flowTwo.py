2e233963bc5d147378f3e6d4d9ee05b0b46df80c
SamYuan1990 <yy19902439@126.com>
2025-07-29 21:35:13 +0800

    enable CI
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- flowTwo.py
+++ flowTwo.py
@@ -4,6 +4,9 @@ from clientInfo import clientInfo
 def flowtwo(json_todo_list, reserved_word,doc_folder, clientInfo, force_refresh: bool = True):
     total = len(json_todo_list["todo"])
     i = 0
+    if clientInfo.get_dryRun:
+        log("dry Run model skip")
+        return 
     for item in json_todo_list['todo']:
         log("processing...one file")
         translate_element(reserved_word, doc_folder, item, clientInfo, force_refresh)
